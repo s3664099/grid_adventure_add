248f56a49d19215d06cb24d499b7d7466a8054e4
David Sarkies <david.sarkies@internode.on.net>
2025-12-07 14:51:30 +1100

    Removed game related code from Player class

--- game/Player.java
+++ game/Player.java
@@ -1,10 +1,10 @@
 /*
-Title: Island of Secrets Initialise Game Class
-Author: Jenny Tyler & Les Howarth
+Title: <Game Name> Initialise Game Class
+Author: 
 Translator: David Sarkies
-Version: 5.0
-Date: 3 December 2025
-Source: https://archive.org/details/island-of-secrets_202303
+Version: 0.1
+Date: 7 December 2025
+Source: 
 */
 
 package game;
@@ -12,7 +12,6 @@ package game;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,28 +28,20 @@ public class Player implements Serializable {
 	private static final long serialVersionUID = 495300605316911022L;
 	private static final Logger logger = Logger.getLogger(Player.class.getName());
 	
-	private int room = 23;
+	private int room = Constants.START_LOCATION;
 	private int roomToDisplay = this.room;
 	private final Map<String,Object> stats = new HashMap<>();
-	private final Random rand = new Random();
-	private enum PlayerState { NORMAL,SWIMMING };
+	private enum PlayerState { NORMAL };
 	private PlayerState playerState = PlayerState.NORMAL;
-	private Swimming swim;
 		
-	private static final int RANDOM_ROOM_TRIGGER = 20;
-	
     /**
      * Creates a new player with default starting stats.
      */
 	public Player() {
 		
-		//Initialize Stats
-		stats.put("strength", Constants.STARTING_STRENGTH);			
-		stats.put("wisdom", Constants.STARTING_WISDOM);				
+		//Initialize Stats			
 		stats.put("timeRemaining",Constants.STARTING_TIME);		
 		stats.put("weight", 0);
-		stats.put("food",2);
-		stats.put("drink", 2);
 	}
 	
     /**
@@ -61,25 +52,7 @@ public class Player implements Serializable {
 	public int getDisplayRoom() {
 		return this.roomToDisplay;
 	}
-	
-    /**
-     * Updates and returns the room to display.
-     * If the player is in the {@link #RANDOM_ROOM_TRIGGER}, 
-     * they are shown a random room instead.
-     *
-     * @return the updated display room
-     */
-	public int updateDisplayRoom() {
 		
-		this.roomToDisplay = this.room;
-		
-		if (this.room == RANDOM_ROOM_TRIGGER) {
-			this.roomToDisplay = rand.nextInt(Constants.NUMBER_OF_ROOMS-1)+1;
-		}
-		
-		return this.roomToDisplay;
-	}
-	
     /**
      * Applies turn-based updates to the player's stats.
      * Decreases time remaining and reduces strength based on weight carried.
@@ -89,23 +62,8 @@ public class Player implements Serializable {
 		int timeRemaining = (int) stats.get("timeRemaining");
 		stats.put("timeRemaining", timeRemaining-1);
 		logger.info(stats.get("timeRemaining").toString());
-		
-		logger.info(stats.get("strength").toString());
-		float strength = (float) stats.get("strength");
-		int weight = (int) stats.get("weight");
-		stats.put("strength", strength - (weight/Constants.NUMBER_OF_ITEMS+0.1f));
-		logger.info(stats.get("strength").toString());
 	}
 	
-    /**
-     * Gets a combined value of strength and wisdom.
-     *
-     * @return strength + wisdom
-     */
-	public float getStrengthWisdon() {
-		return ((float) stats.get("strength"))+((int) getStat("wisdom"));
-	}
-
     /** @return the current room number */
 	public int getRoom() {
 		return this.room;
@@ -144,20 +102,6 @@ public class Player implements Serializable {
 		stats.put(statName,value);
 	}
 	
-    /** @return the {@link Swimming} instance if player is swimming */
-	public Swimming getSwimming() {
-		return swim;
-	}
-	
-    /**
-     * Sets the player's swimming state handler.
-     *
-     * @param swim the swimming instance
-     */
-	public void setSwimming(Swimming swim) {
-		this.swim = swim;
-	}
-	
     /**
      * Reduces a named stat by 1.
      *
@@ -173,13 +117,6 @@ public class Player implements Serializable {
     /** Sets the player back to the {@link PlayerState#NORMAL} state. */
 	public void setPlayerStateNormal() {
 		playerState = PlayerState.NORMAL;
-		swim = null;
-	}
-	
-    /** Puts the player into the {@link PlayerState#SWIMMING} state. */
-	public void setPlayerStateSwimming() {
-		playerState = PlayerState.SWIMMING;
-		setSwimming(new Swimming());
 	}
 	
     /**
@@ -190,16 +127,7 @@ public class Player implements Serializable {
 	public boolean isPlayerStateNormal() {
 		return playerState == PlayerState.NORMAL;
 	}
-	
-    /**
-     * Checks if the player is in the {@link PlayerState#SWIMMING} state.
-     *
-     * @return true if swimming
-     */
-	public boolean isPlayerStateSwimming() {
-		return playerState == PlayerState.SWIMMING;
-	}		
-		
+			
     /** @return formatted string of strength and wisdom */
 	public String toStringStatus() {
 		return String.format("Strength: %.2f         wisdom: %d", stats.get("strength"),stats.get("wisdom"));
@@ -215,44 +143,12 @@ public class Player implements Serializable {
 	public String toString() {
 	    return "Player{" +
 	            "room=" + room +
-	            ", strength=" + stats.get("strength") +
-	            ", wisdom=" + stats.get("wisdom")  +
 	            ", timeRemaining=" + stats.get("timeRemaining")  +
 	            ", weight=" + stats.get("weight") +
-	            ", food=" + stats.get("food") +
-	            ", drink=" + stats.get("drink") +
 	            '}';
 	}
-	
 }
 
-/* 2 November 2024 - Create File
- * 3 November 2024 - Added methods to manipulate the value of the room
- * 				   - Added method to return the player status as a string
- * 5 November 2024 - Added code to randomise location is at room 20
- * 13 November 2024 - Added method to update current room
- * 17 November 2024 - Added getters & setters
- * 25 November 2024 - Added flag for give (though will need to be changed for other screens)
- * 29 November 2024 - Added getter for drink
- * 1 December 2024 - Added adjust function for strength/wisdom/food/drink
- * 9 December 2024 - Made class serializable
- * 16 December 2024 - Added methods to handle swimming in poisoned waters section
- * 23 December 2024 - Updated to version 2.
- * 1 January 2025 - Removed the changeRoom method as duplicates setRoom.
- * 31 January 2025 - Completed Testing and increased version
- * 1 February 2025 - Added serializable ID and removed unused variables.
- * 5 March 2025 - Increased to v4.0
- * 11 March 2025 - Fixed issues and moved stats to a map
- * 12 March 2025 - Removed timeRemaining stats. Removed wisdom, strength, weight getters & setters
- * 14 March 2025 - Removed food & Drink
- * 15 March 2025 - Removed Swimming and added check if swimming. Added toString method.
- * 17 March 2025 - Added logging to list room player has entered.
- * 25 March 2025 - Added Enums Player State. Removed Message and Lightning states (should be in game)
- * 27 March 2025 - Added Swimming class to store swimming state. Created Start Swimming state
- * 25 June 2025 - Restored strength to correct starting value. Moved starting constants to the constants file
- * 19 July 2025 - Removed start swimming state
- * 20 July 2025 - Removed setting the swim position. Removed START_SWIM state
- * 19 August 2025 - Made minor fixes. Added JavaDocs
- * 18 October 2025 - Added logger for when stat changes
- * 3 December 2025 - Increased version number
+/* 3 December 2025 - Created File
+ * 7 December 2025 - Cleared Game Related code
  */
